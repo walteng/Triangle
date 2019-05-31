7926347a0f244159034e4b68baf7fad02c2beff2
walteng <ngetichwalt49@gmail.com>
2019-05-31 17:20:32 +0300

    formated spacing and indentation for the js file and the bootstrap.css

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,15 +3,15 @@ function triangle() {
   var height = parseInt(document.getElementById("height").value);
   var hypotenuse = parseInt(document.getElementById("hypo").value);
 
-  if (base + height > hypotenuse && base + hypotenuse > height && hypotenuse + height > base){
-  if (base == height && base == hypotenuse) {
-    alert("Equilateral");
-  } else if (base === height || height === hypotenuse || base === hypotenuse) {
-    alert("isosceles");
-  } else if (base != height && height != hypotenuse && base != hypotenuse) {
-    alert("scalene");
-  }
-} else{
+  if (base + height > hypotenuse && base + hypotenuse > height && hypotenuse + height > base) {
+    if (base == height && base == hypotenuse) {
+      alert("Equilateral");
+    } else if (base === height || height === hypotenuse || base === hypotenuse) {
+      alert("isosceles");
+    } else if (base != height && height != hypotenuse && base != hypotenuse) {
+      alert("scalene");
+    }
+  } else {
     alert("Not a Triangle");
   }
 };
