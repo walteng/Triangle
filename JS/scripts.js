bd534ad7f835165ee880a045f51c8ac7555beda5
walteng <ngetichwalt49@gmail.com>
2019-05-31 17:18:11 +0300

    created new file for bootstrap.css

---
+++ JS/scripts.js
@@ -0,0 +1,17 @@
+function triangle() {
+  var base = parseInt(document.getElementById("base").value);
+  var height = parseInt(document.getElementById("height").value);
+  var hypotenuse = parseInt(document.getElementById("hypo").value);
+
+  if (base + height > hypotenuse && base + hypotenuse > height && hypotenuse + height > base){
+  if (base == height && base == hypotenuse) {
+    alert("Equilateral");
+  } else if (base === height || height === hypotenuse || base === hypotenuse) {
+    alert("isosceles");
+  } else if (base != height && height != hypotenuse && base != hypotenuse) {
+    alert("scalene");
+  }
+} else{
+    alert("Not a Triangle");
+  }
+};
